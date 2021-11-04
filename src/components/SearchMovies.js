1e7df91808efc797d065005847141afece02233c
Christine Banlawi <christine.banlawi@gmail.com>
2021-11-04 14:55:55 -0300

    Added styling, created Movie Card component, and finished project

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import MovieCard from './MovieCard'
 
 function SearchMovies(props) {
     const [query, setQuery] = useState("")
@@ -38,15 +39,7 @@ function SearchMovies(props) {
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + " poster"} />
-                        <div className="card--content">
-                            <h3 className="card--title">{movie.title}</h3>
-                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
-                            <p className="card--description">{movie.overview}</p>
-                        </div>
-                    </div>
+                    <MovieCard movie={movie} key={movie.id}/>
                 ))}
             </div>
         </>
